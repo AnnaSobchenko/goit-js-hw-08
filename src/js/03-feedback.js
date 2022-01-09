368725c7397747a0801ddf127f58b2b0273c20c8
AnnaSobchenko <55787684+AnnaSobchenko@users.noreply.github.com>
2022-01-09 23:33:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+const { throttle } = require('lodash');
 
+const formEl = document.querySelector('.feedback-form');
+
+loadData(formEl);
+
+formEl.addEventListener('input', throttle(onInputData, 500));
+formEl.addEventListener('submit', onSubmitData);
+
+function loadData(form) {
+  let feedback = localStorage.getItem('feedback-form-state');
+  if (feedback) {
+    feedback = JSON.parse(feedback);
+    [...form.elements].forEach(elem => {
+      if ((elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') && feedback[elem.name]) {
+        elem.value = feedback[elem.name];
+      }
+    });
+  }
+}
+
+function saveData(target) {
+  if (target) {
+    const data = {};
+    [...target.elements].forEach(elem => {
+      if (elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') {
+        data[elem.attributes.name.value] = elem.value;
+      }
+    });
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }
+}
+
+function onInputData(e) {
+  if (e.currentTarget) {
+    saveData(e.currentTarget);
+  }
+}
+
+function onSubmitData(e) {
+  e.preventDefault();
+  for (const elem of [...e.currentTarget.elements]) {
+    if ((elem.nodeName === 'INPUT' || elem.nodeName === 'TEXTAREA') && elem.value === '') {
+      alert('Empty fields!');
+      return;
+    }         
+  }
+
+  saveData(e.currentTarget);
+  e.currentTarget.reset();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state')
+}
